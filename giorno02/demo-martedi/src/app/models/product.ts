857323044f28a50d9b20f728f27acc397aeddb3c
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2019-04-16 12:02:33 +0200

    aggiunta immagine prodotto

--- giorno02/demo-martedi/src/app/models/product.ts
+++ giorno02/demo-martedi/src/app/models/product.ts
@@ -4,12 +4,14 @@ export class Product {
   category: string;
   price: number;
   description: string;
+  image: string;
 
- constructor(id: number, name: string, category: string, price: number, description: string) {
-   this.id = id;
-   this.name = name;
-   this.category = category;
-   this.price = price;
-   this.description = description;
- }
+  constructor(id: number, name: string, category: string, price: number, description: string, image: string) {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+    this.price = price;
+    this.description = description;
+    this.image = image;
+  }
 }
--- giorno02/demo-martedi/src/app/products-list/products-list.component.ts
+++ giorno02/demo-martedi/src/app/products-list/products-list.component.ts
@@ -20,9 +20,14 @@ export class ProductsListComponent implements OnInit {
     this.productNumber = this.products.length;
   }
   private creaListaProdotti(): void {
-this.products.push(new Product(1, 'Harry Potter e la pietra filosofale', 'libro', 12, 'fantasy'));
-this.products.push(new Product(2, 'Il silenzio degli innocenti', 'libro', 13, 'romanzo'));
-this.products.push(new Product(3, 'Il rifugio', 'libro', 14, 'romanzo'));
+this.products.push(new Product(1, 'Harry Potter e la pietra filosofale', 'libro', 12, 'fantasy',
+ 'https://images.pottermore.com/bxd3o8b291gf'
+ + '/3SQ3X2km8wkQIsQWa02yOY/25f258f21bdbe5f552a4419bb775f4f0/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg'));
+this.products.push(new Product(2, 'Il silenzio degli innocenti', 'libro', 13, 'romanzo',
+ 'https://images.pottermore.com/bxd3o8b291gf/3SQ3X2km8wkQIsQWa02yOY'
++ '/25f258f21bdbe5f552a4419bb775f4f0/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg'));
+this.products.push(new Product(3, 'Il rifugio', 'libro', 14, 'romanzo', 'https://images.pottermore.com/bxd3o8b291gf/3SQ3X2km8wkQIsQWa02yOY/'
++ '25f258f21bdbe5f552a4419bb775f4f0/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg '));
   }
   ngOnInit() {
   }
