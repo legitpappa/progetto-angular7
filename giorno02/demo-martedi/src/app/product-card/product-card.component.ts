e213e4d821e4bcbf3af4f58d5fe166cf58350a50
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2019-04-16 17:15:49 +0200

    creato component card

--- giorno02/demo-martedi/src/app/product-card/product-card.component.ts
+++ giorno02/demo-martedi/src/app/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IProduct } from '../models/iProduct';
 
 @Component({
   selector: 'app-product-card',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent implements OnInit {
+@Input() product: IProduct;
 
   constructor() { }
 
