df191bfd326e4bf234dbffd71391882ccdd063fc
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2019-04-16 18:01:30 +0200

    card

--- giorno02/demo-martedi/src/app/products-list/data.ts
+++ giorno02/demo-martedi/src/app/products-list/data.ts
@@ -19,8 +19,7 @@ export const IPRODUCTS: IProduct[] = [
     description: 'libro',
     price: 12,
     category: 'romanzo',
-    image: 'https://images.pottermore.com/bxd3o8b291gf'
-      + '/3SQ3X2km8wkQIsQWa02yOY/25f258f21bdbe5f552a4419bb775f4f0/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg'
+    image: '../../assets/harry.jpg'
   },
   {
     id: 2,
@@ -28,7 +27,7 @@ export const IPRODUCTS: IProduct[] = [
     description: 'libro',
     price: 13,
     category: 'fantasy',
-    image: 'https://i.redd.it/6lbx53qz7sn11.jpg'
+    image: '../../assets/doge.jpg'
   },
   {
     id: 3,
@@ -36,8 +35,7 @@ export const IPRODUCTS: IProduct[] = [
     description: 'libro',
     price: 14,
     category: 'romanzo',
-    image: 'https://images.pottermore.com/bxd3o8b291gf'
-      + '/3SQ3X2km8wkQIsQWa02yOY/25f258f21bdbe5f552a4419bb775f4f0/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg'
+    image: '../../assets/harry.jpg'
   }
 ];
 
